aa3ba4f27a2ebf3c439f2141c244048465a39931
WillianNalbert <williannalbert@hotmail.com>
2023-01-24 23:38:34 -0300

    Iniciando repositório no git

---
+++ BookManagerAPI/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookManagerAPI.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Autor { get; set; }
+        public string Descricao { get; set; }
+        public string Title { get; set; }
+    }
+}
